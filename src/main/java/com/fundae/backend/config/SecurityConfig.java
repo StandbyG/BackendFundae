a99b44dbfe9aa4a892ff2f6890fcf45a2a3832a2
Gabriel Meza Prada <standbyg2@gmail.com>
2025-09-03 18:04:41 -0500

    probably last commit

--- src/main/java/com/fundae/backend/config/SecurityConfig.java
+++ src/main/java/com/fundae/backend/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://frabjous-valkyrie-b2b9cf.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
